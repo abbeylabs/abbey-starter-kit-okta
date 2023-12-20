2b4496501b12189a82b6875728142c19182e059b
Jeff Chao <jeffchao@me.com>
2023-12-20 14:59:10 -0800

    Use new references API, update TF provider.

--- main.tf
+++ main.tf
@@ -1,34 +1,9 @@
-terraform {
-  backend "http" {
-    address        = "https://api.abbey.io/terraform-http-backend"
-    lock_address   = "https://api.abbey.io/terraform-http-backend/lock"
-    unlock_address = "https://api.abbey.io/terraform-http-backend/unlock"
-    lock_method    = "POST"
-    unlock_method  = "POST"
-  }
+locals {
+  account_name = ""
+  repo_name = ""
 
-  required_providers {
-    abbey = {
-      source = "abbeylabs/abbey"
-      version = "0.2.4"
-    }
-
-    okta = {
-      source  = "okta/okta"
-      version = "~> 4.0.2"
-    }
-  }
-}
-
-provider "abbey" {
-  # Configuration options
-  bearer_auth = var.abbey_token
-}
-
-provider "okta" {
-  # Configuration options
-  org_name  = var.okta_org_name
-  base_url  = var.okta_base_url
+  project_path = "github://${local.account_name}/${local.repo_name}"
+  policies_path = "${local.project_path}/policies"
 }
 
 resource "abbey_grant_kit" "okta_group_has_nice_things" {
@@ -47,29 +22,17 @@ resource "abbey_grant_kit" "okta_group_has_nice_things" {
     ]
   }
 
+  policies = [
+    { bundle = local.policies_path }
+  ]
+
   output = {
-    # Replace with your own path pointing to where you want your access changes to manifest.
-    # Path is an RFC 3986 URI, such as `github://{organization}/{repo}/path/to/file.tf`.
-    location = "github://replace-me-with-organization/replace-me-with-repo/access.tf" # CHANGEME
+    location = "${local.project_path}/access.tf"
     append = <<-EOT
-      resource "okta_user_group_memberships" "has_nice_things__{{ .data.system.abbey.identities.okta.user_id }}" { # {{ .data.system.abbey.identities.abbey.email }}
-        user_id = "{{ .data.system.abbey.identities.okta.user_id }}"
-        groups = ["${data.okta_group.has_nice_things.id}"]
+      resource "okta_user_group_memberships" "has_nice_things__{{ .user.okta.user_id }}" { # {{ .user.email }}
+        user_id = "{{ .user.okta.user_id }}"
+        groups = [data.okta_group.has_nice_things.id]
       }
     EOT
   }
-}
-
-resource "abbey_identity" "user_1" {
-  abbey_account = "replace-me@example.com"
-  source = "okta"
-  metadata = jsonencode(
-    {
-      user_id = "00uReplaceWithOktaUserId" #CHANGEME
-    }
-  )
-}
-
-data "okta_group" "has_nice_things" {
-  name = "Has nice things"
 }
